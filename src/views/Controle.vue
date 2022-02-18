6a70e0b84187a9966fedc0834ade153b863fc8c8
kuuchan-code <kuuchanxn@gmail.com>
2022-02-19 02:12:49 +0900

    new file:   src/views/Controle.vue

---
+++ src/views/Controle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="controle">
+    <h1>This is an controle page</h1>
+
+    <div class="container-sm">
+      <div class="d-grid gap-2 col-6 mx-auto">
+        <input id="set-time" class="form-control-lg" style="text-align: center" type="time" />
+
+        <button type="button" class="btn btn-primary btn-lg" id="go-forward">
+          <i class="bi bi-chevron-up"></i> 前進
+        </button>
+        <button type="button" class="btn btn-primary btn-lg" id="go-backward">
+          <i class="bi bi-chevron-down"></i> 後進
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
