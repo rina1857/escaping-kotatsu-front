b6993d012c74fd8f1666b1491a8aebc359233e1e
kuuchan-code <kuuchanxn@gmail.com>
2022-02-18 22:40:17 +0900

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import store from './store';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+/* add icons to the library */
+library.add(faUserSecret);
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
