a1e27114a50883113ef7ba86801ae0628778d22a
kuuchan-code <kuuchanxn@gmail.com>
2022-02-23 16:00:10 +0900

    modified:   src/views/Controle.vue

--- src/views/Controle.vue
+++ src/views/Controle.vue
@@ -2,10 +2,10 @@
   <div class="controle">
     <h1>逃げるこたつ操作画面</h1>
     <div class="container-sm">
-      <h2><i class="bi bi-alarm"></i> 時間で引っ張る</h2>
       <div class="d-grid gap-2 col-6 mx-auto">
-            <input id="set-time" class="form-control-lg" style="text-align: center" type="time" />
-            <h2><i class="bi bi-play"></i> すぐに引っ張る</h2>
+        <h2><i class="bi bi-alarm"></i> 時間で引っ張る</h2>
+        <input id="set-time" class="form-control-lg" style="text-align: center" type="time" />
+        <h2><i class="bi bi-play"></i> すぐに引っ張る</h2>
         <button
           type="button"
           class="btn btn-primary btn-lg"
